93d97f5e479bb482df2d67a5dfbcd4ac4060537e
no name <120008252+supernuakakeche@users.noreply.github.com>
2022-12-18 11:30:57 +0800

    add exsemple004_div
    it doesn't work from the computer

--- exsemple004_div/Program.cs
+++ exsemple004_div/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+double namberA = new Random ().Next (1, 10);
+Console.WriteLine (namberA);
+double namberB = new Random ().Next (1, 10);
+Console.WriteLine (namberB);
+double result = namberA / namberB;
+Console.WriteLine (result);
