4f96c37a867b54f869cbae70b88c2e2c9657edea
Vladislav <milliganiam6@gmail.ru>
2022-12-21 18:15:15 +0800

    add exsample010_metod_array

---
+++ exsample010_metod_array/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1,26,53,54,15,66,75,84,98};
+
+int n = array.Length;
+int find = 26;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+
+    {
+     Console.WriteLine(index);
+     break;
+    }
+ index = index + 1;
+}
+ Console.WriteLine(index);
